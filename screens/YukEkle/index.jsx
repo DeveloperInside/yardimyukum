419bf78ca768d5478652e86e0271bde384b04571
DeveloperInside <enesm.hancer@gmail.com>
2023-02-16 12:16:12 +0300

    test: test data added

--- screens/YukEkle/index.jsx
+++ screens/YukEkle/index.jsx
@@ -1,21 +1,37 @@
-import { View, SafeAreaView } from 'react-native'
+import { View, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 import commonStyles from 'styles/commonStyles'
-import { Text, Input, Button, Spacer } from 'components/componentList'
-import { FAB } from 'react-native-paper'
+import { Text, Fab, LoadListItem, LoadModal } from 'components/componentList'
 import styles from './styles'
 
 const YukEkle = () => {
+  const testData = [
+    {
+      class: 'Isınma',
+      name: 'Battaniye',
+      count: '900'
+    },
+    {
+      class: 'Gıda',
+      name: 'Kuru Gıda',
+      count: '7000'
+    },
+    {
+      class: 'İhtiyaç',
+      name: 'Bebek bezi',
+      count: '800'
+    }
+  ]
+
   return (
     <SafeAreaView style={commonStyles.container}>
       <View>
         <Text>YukEkle</Text>
       </View>
-      <FAB
-        icon="plus"
-        style={styles.fab}
-        onPress={() => console.log('Pressed')}
-      />
+      <LoadListItem item={testData[0]} />
+      <FlatList data={testData} renderItem={LoadListItem}/>
+      <Fab style={styles.fab} onPress={() => console.log('Pressed')} />
+      <LoadModal item={testData[0]} isOpen={false}/>
     </SafeAreaView>
   )
 }
