7c84bb189aae4a97e4b5635312b21676ef8a270d
Metehan Geniş <mthngns78@gmail.com>
2023-02-18 17:59:17 +0300

    CHORE: Test files created

---
+++ testDB.js
@@ -0,0 +1,22 @@
+const testData = [
+  {
+    product_class: 'Isınma',
+    product_name: 'Battaniye',
+    count: '900',
+    annotation: 'not'
+  },
+  {
+    product_class: 'Gıda',
+    product_name: 'Kuru Gıda',
+    count: '7000',
+    annotation: 'not'
+  },
+  {
+    product_class: 'İhtiyaç',
+    product_name: 'Bebek bezi',
+    count: '800',
+    annotation: 'not'
+  }
+]
+
+export default testData
