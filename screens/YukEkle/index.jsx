fb9fca80a58aeb8354fee46410e70385ae5434f9
Metehan Geniş <mthngns78@gmail.com>
2023-02-18 17:55:48 +0300

    FIX:Input keyboard type

--- screens/YukEkle/index.jsx
+++ screens/YukEkle/index.jsx
@@ -3,31 +3,15 @@ import React from 'react'
 import commonStyles from 'styles/commonStyles'
 import { Text, Fab, LoadListItem, LoadModal } from 'components/componentList'
 import styles from './styles'
+import testData from 'testDB'
 
 const YukEkle = () => {
-  const testData = [
-    {
-      class: 'Isınma',
-      name: 'Battaniye',
-      count: '900'
-    },
-    {
-      class: 'Gıda',
-      name: 'Kuru Gıda',
-      count: '7000'
-    },
-    {
-      class: 'İhtiyaç',
-      name: 'Bebek bezi',
-      count: '800'
-    }
-  ]
-
   const renderItem = ({ item }) => {
     return (
       <LoadListItem
-        item={item}
-        onPress={abc => {
+        name={item.product_name}
+        count={item.count}
+        onPress={() => {
           console.log(item)
         }}
       />
